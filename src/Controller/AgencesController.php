bbc8fee22c5dae613eeacd6dcb1dc310474dd6ad
Diana Raharinosy <dianaraharinosy@yahoo.com>
2023-02-10 12:07:12 +0100

    function afficher is done

--- src/Controller/AgencesController.php
+++ src/Controller/AgencesController.php
@@ -10,21 +10,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class AgencesController extends AbstractController
 {
     #[Route('/agences', name: 'app_agences')]
-    public function index(): Response
-    {
-        return $this->render('agences/index.html.twig', [
-            'controller_name' => 'AgencesController',
-        ]);
-    }
-
-
-    #[Route('/agences', name:"afficher")]
-    public function agences_afficher(AgencesRepository $repoAgences)
+    public function index(AgencesRepository $repoAgences): Response
     {
         $agences = $repoAgences->findAll();
 
-        return $this->render("agences/agences_afficher.html.twig", [
-            "agences" => $agences
+        
+
+        return $this->render('agences/index.html.twig', [
+            'controller_name' => 'AgencesController',
+            "agences" => $agences,
         ]);
     }
+
+
+
+    // #[Route('/agences/showAdd', name: "showAdd")]
+    // public function agences_ajouter()
+
+
 }
