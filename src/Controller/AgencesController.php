bbba320b7c928581a8f2bae37f2a26902f107fd4
Diana Raharinosy <dianaraharinosy@yahoo.com>
2023-02-10 10:38:50 +0100

    add function afficher on agences s controller

---
+++ src/Controller/AgencesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AgencesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AgencesController extends AbstractController
+{
+    #[Route('/agences', name: 'app_agences')]
+    public function index(): Response
+    {
+        return $this->render('agences/index.html.twig', [
+            'controller_name' => 'AgencesController',
+        ]);
+    }
+
+
+    #[Route('/agences', name:"category_afficher")]
+    public function agences_afficher(AgencesRepository $repoAgences)
+    {
+        $agences = $repoAgences->findAll();
+
+        return $this->render("agences/agences_afficher.html.twig", [
+            "agences" => $agences
+        ]);
+    }
+}
