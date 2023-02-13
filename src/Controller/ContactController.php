e0ec456eb8c59603c7121b83db345aabb5d667cb
Herve Kilanda <kilandaherve@gmail.com>
2023-02-13 13:42:18 +0100

    contact page

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,20 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use App\Form\ContactType;
+use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 
+
+
 class ContactController extends AbstractController
 {
-    // #[Route('/contact', name: 'app_contact')]
-    // public function index(): Response
-    // {
-    //   $form = 
-    // }
+    #[Route('/contact', name: 'app_contact')]
+    public function index(ContactRepository $contact)
+    {   
+        $contact = new Contact;
+        $form = $this->createForm(ContactType::class, $contact);
+
+      return $this->render('contact/index.html.twig',[
+              'form' => $form->createView()
+      ]);
+    }
+   
+   
 }
 
 
