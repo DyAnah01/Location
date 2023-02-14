54daea7b4c8136a0972e1c4fab38b312194483ac
Diana Raharinosy <dianaraharinosy@yahoo.com>
2023-02-14 16:46:19 +0100

    update index in accueil for INSERT INTO

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,16 +2,19 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $manager): Response
     {
         
+
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
         ]);
