7f6846cdc34ca8cf24eef4c1dcd1a0167325fe2d
Diana Raharinosy <dianaraharinosy@yahoo.com>
2023-02-14 16:02:14 +0100

    start with show commande but switch to add commande

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\CommandeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommandeController extends AbstractController
 {
     #[Route('/commande', name: 'app_commande')]
-    public function index(): Response
+    public function index(CommandeRepository $repoCo): Response
     {
+        $commande = $repoCo->findAll();
+
         return $this->render('commande/index.html.twig', [
             'controller_name' => 'CommandeController',
+            'commande' => $commande,
         ]);
     }
 }
