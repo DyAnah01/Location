e9236860093d92d70e5a0dce7bd507066d4ef890
Diana Raharinosy <dianaraharinosy@yahoo.com>
2023-02-14 09:20:09 +0100

    add RegistrationController

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ListController extends AbstractController
+class RegistrationController extends AbstractController
 {
-    #[Route('/list', name: 'app_list')]
+    #[Route('/registration', name: 'app_registration')]
     public function index(): Response
     {
-        return $this->render('list/index.html.twig', [
-            'controller_name' => 'ListController',
+        return $this->render('registration/index.html.twig', [
+            'controller_name' => 'RegistrationController',
         ]);
     }
 }
